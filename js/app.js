d8885f11585ea53cd45be0861c2d1b9b110fbad5
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-04-29 20:31:24 -1000

    Star Wars API practice

---
+++ js/app.js
@@ -0,0 +1,33 @@
+
+
+var world = document.createElement ("p");
+world.innerHTML = 'Hello there.';
+document.body.appendChild (world);
+
+function getData (){
+
+var random = Math.floor (Math.random()*87);
+var swApi = 'https://swapi.co/api/people/' + random;
+
+axios.get(swApi).then(function(response){
+    console.log (response.data);
+    showData (response.data);
+})
+
+}
+
+function showData (data){
+    var divElem = document.createElement (
+        'div');
+    divElem.id = 'character';
+    divElem.innerHTML = data.name;
+    profile.appendChild (divElem);
+
+
+    var pElem = document.createElement ('p');
+    pElem.id = 'weight';
+    pElem.innerHTML = data.mass
+    profile.appendChild (pElem);
+}
+
+getData();
